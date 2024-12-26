230c2d88aa18cf3ca2689cd0c3d230a9c02d8328
skc <crisychicken@sjtu.edu.cn>
2024-12-26 12:42:54 +0800

    first commit

---
+++ Core/Src/main.c
@@ -0,0 +1,277 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <string.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+typedef enum {
+  STATE_STOPPED,
+  STATE_STARTING,
+  STATE_STARTED
+} SystemState;
+
+#define RX_BUF_LEN  256     //
+typedef struct
+{
+    unsigned char rx_buf[RX_BUF_LEN];
+    uint16_t data_length;
+} rxStruct;
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+uint8_t aRxBuffer2;			
+rxStruct rxS2 = {
+        .rx_buf = {0}, 
+        .data_length = 0 
+};
+uint8_t aRxBuffer3;			
+
+SystemState currentState = STATE_STOPPED;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_USART3_UART_Init();
+  MX_TIM1_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_UART_Receive_IT(&huart2, (uint8_t *)&aRxBuffer2, 1); //
+  HAL_UART_Receive_IT(&huart3, (uint8_t *)&aRxBuffer3, 1); //
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    if (currentState == STATE_STARTED){
+      if (rxS2.data_length > 0)
+      {
+        __HAL_UART_DISABLE_IT(&huart2, UART_IT_RXNE);
+        HAL_UART_Transmit(&huart3, (uint8_t *)rxS2.rx_buf, rxS2.data_length, 0xFFFF); //
+        rxS2.data_length = 0;
+        memset(rxS2.rx_buf, 0x00, sizeof(rxS2.rx_buf));
+        __HAL_UART_ENABLE_IT(&huart2, UART_IT_RXNE);
+      }
+    }
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+//TIM定时返回函数
+void HAL_TIM_PeriodElapsedCallback (TIM_HandleTypeDef *htim)//回调函数
+{
+ 
+	if(htim->Instance == TIM1)//判断进入定时1通道回调函数
+	{
+    __HAL_UART_DISABLE_IT(&huart2, UART_IT_RXNE);
+    if (currentState == STATE_STARTING) {
+      currentState = STATE_STARTED;
+      __HAL_TIM_CLEAR_FLAG(&htim1, TIM_FLAG_UPDATE);//启用前清除TIM中断标志�???
+      __HAL_TIM_CLEAR_IT(&htim1, TIM_IT_UPDATE);//启用前清除TIM中断
+      __HAL_TIM_SET_COUNTER(&htim1, 0); // 重置定时
+      __HAL_TIM_SET_AUTORELOAD(&htim1, 10000-1); // 设置定时周期1s
+      HAL_TIM_Base_Start_IT(&htim1);//�???启TIM计数
+    }
+    if (currentState == STATE_STARTED) {
+      currentState = STATE_STOPPED;
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET); // 关闭PA5引脚
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET);
+    }
+    __HAL_UART_ENABLE_IT(&huart2, UART_IT_RXNE);
+  }
+}
+
+/* USER CODE BEGIN 0 */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+  /* Prevent unused argument(s) compilation warning */
+  UNUSED(huart);
+  /* NOTE: This function Should not be modified, when the callback is needed,
+           the HAL_UART_TxCpltCallback could be implemented in the user file
+  */
+  if(huart->Instance == USART2)
+  {
+    if (rxS2.data_length >= 255) //
+    {
+      rxS2.data_length = 0;
+      memset(rxS2.rx_buf, 0x00, sizeof(rxS2.rx_buf));
+    }
+    else
+    {
+      rxS2.rx_buf[rxS2.data_length++] = aRxBuffer2; //
+    }
+    HAL_UART_Receive_IT(&huart2, (uint8_t *)&aRxBuffer2, 1); //
+    if (currentState == STATE_STOPPED) {
+      currentState = STATE_STARTING;
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET); // 打开PA5引脚
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET);
+      __HAL_TIM_CLEAR_FLAG(&htim1, TIM_FLAG_UPDATE);//启用前清除TIM中断标志�???
+      __HAL_TIM_CLEAR_IT(&htim1, TIM_IT_UPDATE);//启用前清除TIM中断
+      __HAL_TIM_SET_COUNTER(&htim1, 0); // 重置定时
+      __HAL_TIM_SET_AUTORELOAD(&htim1, 50-1); // 设置定时周期
+      HAL_TIM_Base_Start_IT(&htim1);//�???启TIM计数
+    }
+    if (currentState == STATE_STARTED) {
+      __HAL_TIM_CLEAR_FLAG(&htim1, TIM_FLAG_UPDATE);//清除TIM中断
+      __HAL_TIM_CLEAR_IT(&htim1, TIM_IT_UPDATE);//清除TIM中断
+      __HAL_TIM_SET_COUNTER(&htim1, 0); // 重置定时
+      __HAL_TIM_SET_AUTORELOAD(&htim1, 10000-1); // 设置定时周期
+      HAL_TIM_Base_Start_IT(&htim1);//�???启TIM计数
+    }
+  }
+  if (huart->Instance == USART3)
+  {
+    HAL_UART_Transmit(&huart2, (uint8_t *)&aRxBuffer3, 1, 0xFFFF); //
+    HAL_UART_Receive_IT(&huart3, (uint8_t *)&aRxBuffer3, 1); //
+  }
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
